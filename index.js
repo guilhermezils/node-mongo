1a669ea13410eab91b5b033b092db5ac9a7d659a
guilhermezils <guilhermezils@icloud.com>
2021-07-30 11:09:54 -0500

    3rd step

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 7000 // const port = process.env.PORT || 7000 ** for production/deploying 
 
+//cluster connection/database
 const uri =  "mongodb+srv://guilhermezils:qweasdzxc@cluster0.qnb59.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-app.listen(port, () => console.log(`running on ${port}`))
+app.use(cors())
+app.use(express.json())
+
+
+//easier way to connect
+mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology: true})
+    .then(() => console.log("MongoDB Connected"))
+    .catch(console.error)
+
+
+app.listen(port, () => console.log(`NODE App running on ${port}`))
